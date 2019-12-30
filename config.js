6c40ddf1f7b6da738d0b5de3aaa0dfc230a7cc3d
sush154 <sushant.pj41288@gmail.com>
2019-12-30 16:41:36 +0530

    Fixing vulnerabilities
    Fixing vulnerabilities

--- config.js
+++ config.js
@@ -3,12 +3,13 @@ var allowedOrigins = ['http://localhost:3000', 'http://localhost:8100'];
 
 var config = {
 		mongo : {
-			//connectionUrl : 'mongodb://admin:admin@ds163053.mlab.com:63053/aartiscollection'
-			connectionUrl : 'mongodb://localhost:27017/aartiscollection'
+			connectionUrl : 'mongodb+srv://sush154:Dhundi63@cluster0-phx5x.mongodb.net/test?retryWrites=true&w=majority'
+			// connectionUrl : 'mongodb://localhost:27017/aartiscollection'
 		},
 		mongooseConnection : null,
 		client : {
-			connectionUrl : ['http://localhost:3000', 'http://localhost:8100', 'https://aarticollectionadminui.herokuapp.com/', 'http://localhost:4200']
+			connectionUrl : ['http://localhost:3000', 'http://localhost:8100', 
+			'https://aarticollectionadminui.herokuapp.com/', 'http://localhost:4200']
 		}
 }
 
